369c20b81b731dd41c06763e51c321736ad1eeab
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-05-08 12:12:21 +0530

    Time: 4 ms (36.81%) | Memory: 44.3 MB (31.67%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,21 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Deque<Integer> st=new ArrayDeque<>();
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        
-        for(int i=0;i<nums2.length;i++){
+      HashMap<Integer,Integer> hm=new HashMap<>();
+       Deque<Integer> st=new ArrayDeque<>();
+       int a[]=new int[nums1.length];
+       for(int i=nums2.length-1;i>=0;i--){
             while(!st.isEmpty()&&st.peek()<=nums2[i]){
-                hm.put(st.peek(),nums2[i]);
                 st.pop();
             }
-            st.push(nums2[i]); 
+            if(!st.isEmpty()){
+                hm.put(nums2[i],st.peek());
+            }
+            else{
+                hm.put(nums2[i],-1);
+            }
+            st.push(nums2[i]);
+
+       }
+
+       for(int i=0;i<nums1.length;i++){
+        if(hm.containsKey(nums1[i])){
+            a[i]=hm.get(nums1[i]);
         }
-        while (!st.isEmpty()) {
-            hm.put(st.pop(),-1);
-        }
-        for(int i=0;i<nums1.length;i++){
-            nums1[i]=hm.get(nums1[i]);
-        }
-        return nums1;
+       }
+       return a;
+
     }
 }
