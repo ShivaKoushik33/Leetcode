379265bc664233ac7961d2c63a13794138bc843e
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-08-17 01:44:04 +0530

    Time: 6 ms (7.89%) | Memory: 43 MB (5.75%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public void rec(HashMap<Integer,String> hm,int j,List<Character> l,List<String> ans,int product,HashMap<Integer,Integer> num){
+    public void rec(HashMap<Integer,String> hm,int j,List<Character> l,List<String> ans,HashMap<Integer,Integer> num){
         if(l.size()==num.size()){
             String s="";
             for(char x:l){
@@ -15,7 +15,7 @@ class Solution {
         for(int i=0;i<y.length();i++){
             String x=hm.get(outer);
             l.add(x.charAt(i));
-            rec(hm,j+1,l,ans,product,num);
+            rec(hm,j+1,l,ans,num);
             l.remove(l.size()-1);
         }
     }
@@ -23,13 +23,12 @@ class Solution {
         
         HashMap<Integer,String> hm=new HashMap<>();
         HashMap<Integer,Integer> num=new HashMap<>();
-        
         List<Character> l=new ArrayList<>();
         List<String> ans=new ArrayList<>();
         if(digits.length()==0){
             return ans;
         }
-        int product=1;
+     
          hm.put(2, "abc");
         hm.put(3, "def");
         hm.put(4, "ghi");
@@ -41,13 +40,12 @@ class Solution {
         for(int i=0;i<digits.length();i++){
             int x=digits.charAt(i)-'0';
             // System.out.println(x*2);
-           product*=hm.get(x).length();
             num.put(i,x);
         }
        
         //  rec(hm,j+1,l,ans,product)
         // System.out.println(product+"product");
-        rec(hm,0,l,ans,product,num);
+        rec(hm,0,l,ans,num);
 
         return ans;
 
