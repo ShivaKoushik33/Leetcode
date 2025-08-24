ea76d6e675c2670585b66e36869bf3604b86ad7c
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-08-25 00:06:54 +0530

    Time: 557 ms (62.77%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
-select customer_id from( 
-select customer_id,((count(distinct product_key)/(select count(*) from Product))) as cnt from customer group by customer_id)as derived where cnt=1
+-- select customer_id from( 
+-- select customer_id,((count(distinct product_key)/(select count(*) from Product))) as cnt from customer group by customer_id)as derived where cnt=1
+
+select customer_id from
+customer group by
+customer_id 
+having count(distinct product_key)=(select count(*) from product);
