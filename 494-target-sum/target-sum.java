7526ef39d3a25b21a520be503f9704ec6565f6e2
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-08-29 13:58:10 +0530

    Time: 5 ms (83.53%) | Memory: 44.1 MB (39.38%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -8,6 +8,10 @@ class Solution {
                     return 0;
                 }
             }
+            if(dp[i][target]!=-1){
+                return dp[i][target];
+            }
+            
             if(i>=nums.length){
                 return 0;
             }
@@ -17,10 +21,13 @@ class Solution {
             if(nums[i]<=target){
                  take=fn(i+1,nums,target-nums[i]);
             }
-            return take+leave;
+            return dp[i][target]=take+leave;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int sum=0;
+        if(target<0){
+            target=target*(-1);
+        }
         for(int x:nums){
             sum+=x;
         }
@@ -28,7 +35,13 @@ class Solution {
         if(t-(int)t!=0){
             return 0;
         }
+        
         int val=(target+sum)/2;
+         dp=new int[nums.length+1][val+1];
+         for(int []y:dp){
+            Arrays.fill(y,-1);
+            
+        }
         return fn(0,nums,val);
 
     }
