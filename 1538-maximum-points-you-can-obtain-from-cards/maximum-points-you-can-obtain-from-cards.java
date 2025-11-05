5815213e3260e7c2122577d9b4aed72adf7a1147
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-11-06 01:25:33 +0530

    Time: 4 ms (6.11%) | Memory: 59.8 MB (5.38%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -12,7 +12,7 @@ class Solution {
         int ans=Integer.MIN_VALUE;
         for(int i=1;i<k;i++){
             ans=Math.max(ans,(prefix[i-1]+suffix[k-i-1]));
-            System.out.println(ans);
+            // System.out.println(ans);
         }
         ans=Math.max(prefix[k-1],Math.max(ans,suffix[k-1]));
         return ans;
