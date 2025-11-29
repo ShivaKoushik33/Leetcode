34492413436cff121d42c0608c60696d70f52d76
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-11-29 13:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (6.19%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=1.0;
+        long copy=n;
+        if(copy<0){
+            copy=-1*copy;
+        }
+        while(copy>0){
+            if(copy%2==1){
+                ans=ans*x;
+                copy=copy-1;
+            }
+            else{
+                x=x*x;
+                copy=copy/2;
+            }
+        }
+        if(n<0){
+            ans=(double)1/ans;
+        }
+        return ans;
+    }
+}
