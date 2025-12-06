3bc0fe0bc1dcd0d6569c2ff24b8a71b301bd9939
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-12-06 14:56:42 +0530

    Time: 10 ms (5.23%) | Memory: 46.7 MB (5.46%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,28 +1,25 @@
 class Solution {
-    public void combination(int i,int []candidates,int target,List<List<Integer>>l,List<Integer>p)  
-     {
-        if(i==candidates.length){
-            if(target==0){
-                // System.out.println(p.toString());
-                l.add(new ArrayList<>(p));
-            }
+    public void fn(int i,int arr[],List<Integer> l,List<List<Integer>> ans,int target){
+        if(target==0){
+            System.out.println(l);
+            ans.add(new ArrayList<>(l));
+            return;
+           
+        }
+        if(i>=arr.length){
             return ;
         }
-        if((candidates[i]<=target)){
-            p.add(candidates[i]);
-            combination(i,candidates,target-candidates[i],l,p);
-            p.remove(p.size()-1);
-            
-
+        if(arr[i]<=target){
+            l.add(arr[i]);
+            fn(i,arr,l,ans,target-arr[i]);
+            l.remove(l.size()-1);
         }
-        combination(i+1,candidates,target,l,p);
+        fn(i+1,arr,l,ans,target);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        int n=candidates.length;
-        List<List<Integer>> l=new ArrayList<>();
-        List<Integer> p=new ArrayList<>();
-        combination(0,candidates,target,l,p);
-
-    return l;
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> l=new ArrayList<>();
+        fn(0,candidates,l,ans,target);
+        return ans;
     }
 }
