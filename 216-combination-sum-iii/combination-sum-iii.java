1396fe6c8a71c1182722cd3129f4246bf1e9187e
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2025-12-08 11:55:42 +0530

    Time: 1 ms (21.98%) | Memory: 42.5 MB (24.32%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -21,11 +21,29 @@ class Solution {
         k=k+1;
         rec(i+1,arr,target,ans,l,k);
     }
+
+
+    public void fn(int ind,int k,int n,int a[],List<List<Integer>> ans,List<Integer> l){
+        if((k==0 &&n==0)){
+            ans.add(new ArrayList<>(l));
+            return ;
+        }
+        if((k==0 &&n!=0)||(k!=0 &&n==0)){
+            return ;
+        }
+
+        for(int i=ind;i<a.length;i++){
+            l.add(a[i]);
+            fn(i+1,k-1,n-a[i],a,ans,l);
+            l.remove(l.size()-1);
+        }
+    }
     public List<List<Integer>> combinationSum3(int k, int n) {
         int a[]={1,2,3,4,5,6,7,8,9};
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> l=new ArrayList<>();
-        rec(0,a,n,ans,l,k);
+        // rec(0,a,n,ans,l,k);
+        fn(0,k,n,a,ans,l);
         return ans;
     }
 }
